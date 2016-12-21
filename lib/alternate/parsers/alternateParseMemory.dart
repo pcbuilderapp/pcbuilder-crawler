2b9f3610cb0ed5dabdb22c161e63bf283d7bb0f2
Daniel de Witte <danieldewitte@gmail.com>
2016-12-21 23:50:39 +0100

    updated MemoryParser to not have buildform in its name, and exclude SO-DIMMS from crawling since they are mostly used for laptops

--- lib/alternate/parsers/alternateParseMemory.dart
+++ lib/alternate/parsers/alternateParseMemory.dart
@@ -19,8 +19,9 @@ class AlternateMemoryParser implements PageWorker {
       memory.price = price(listRow.querySelector("span.price").text);
       memory.shop = "Alternate";
       await Crawler.crawl(memory.url, new AlternateMemoryDetailParser(), arguments: memory);
-
-      memoryUnits.add(memory);
+      if (memory.connectors.length > 0 ) {
+        memoryUnits.add(memory);
+      }
     }
     return memoryUnits;
   }
@@ -37,7 +38,6 @@ class AlternateMemoryDetailParser implements PageWorker {
     product.mpn = dataFlix.attributes["data-flix-mpn"];
 
     String memType = "";
-    String memForm = "";
     var techDataTableElements = document.querySelectorAll("div.techData table tr");
     for (int i = 0; i < techDataTableElements.length; i++) {
       String techDataLabel = techDataTableElements[i].querySelector("td.c1").text.trim();
@@ -45,10 +45,11 @@ class AlternateMemoryDetailParser implements PageWorker {
       if (techDataLabel == "Type") {
         memType = techData;
       } else if (techDataLabel == "Bouwvorm") {
-        memForm = techData;
+        if (techData.trim() != "SO-DIMM") {
+          product.connectors.add(new Connector(memType, "MEM"));
+        }
       }
     }
-    product.connectors.add(new Connector(memType + " " + memForm, "MEM"));
 
     String productJSON = new JsonEncoder.withIndent("  ").convert(product);
     postRequest(getBackendServerURL()+"/product/add", productJSON);
