f88b411afae56adaebfa6381635eadfa6b18754a
Stefan Ladage <sladage@gmail.com>
2017-01-11 21:32:43 +0100

    Simple scheduler implemented.

--- bin/pcbuildercrawler.dart
+++ bin/pcbuildercrawler.dart
@@ -1,7 +1,14 @@
 import 'package:pcbuilder.crawler/alternate/crawlternate.dart';
 import 'package:pcbuilder.crawler/informatique/informacrawl.dart';
+import 'dart:io';
+import 'dart:async';
 
 main(List<String> args) async {
-  Informacrawl.crawlInformatique();
-  //Crawlternate.crawlAlternate();
+  new Timer.periodic(new Duration(minutes: 1),(Timer timer){
+    DateTime currentTime = new DateTime.now();
+    if (currentTime.hour == 4) {
+      Informacrawl.crawlInformatique();
+      Crawlternate.crawlAlternate();
+    }
+  });
 }
