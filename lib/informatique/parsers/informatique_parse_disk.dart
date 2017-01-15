ca56eb5dc7ece4d769561805c37f01cffee5517e
Mehmet Sozen <sozen.m@hotmail.com>
2017-01-16 00:39:05 +0100

    Bugfixes Informatique Parse Disk, now It crawles all harddisks.

--- lib/informatique/parsers/informatique_parse_disk.dart
+++ lib/informatique/parsers/informatique_parse_disk.dart
@@ -4,21 +4,17 @@ import "package:pcbuilder.crawler/utils.dart";
 import "package:pcbuilder.crawler/crawler.dart";
 
 class InformatiqueDiskParser implements PageWorker {
-
   parse(Document document, arguments) async {
-
     var rows = document.querySelectorAll("ul.novendorlogo");
 
     for (Element listRow in rows) {
-
       var productRows = listRow.querySelectorAll("li");
 
-      for (Element productRow in productRows){
-
+      for (Element productRow in productRows) {
         Product disk = new Product();
 
         var querySelector = productRow.querySelector(".product_overlay");
-        if (querySelector == null ){
+        if (querySelector == null) {
           continue;
         }
 
@@ -27,22 +23,22 @@ class InformatiqueDiskParser implements PageWorker {
         disk.type = "STORAGE";
         disk.shop = "Informatique";
 
-        await Crawler.crawl(disk.url, new InformatiqueDiskDetailParser(), arguments: disk);
+        await Crawler.crawl(disk.url, new InformatiqueDiskDetailParser(),
+            arguments: disk);
       }
     }
   }
 }
 
 class InformatiqueDiskDetailParser implements PageWorker {
-
   parse(Document document, arguments) async {
-
     Product disk = arguments as Product;
 
     disk.brand = document.querySelector("span[itemprop='brand']").text;
     disk.price = price(document.querySelector("p.verkoopprijs").text);
 
-    var prodImgA = document.querySelector("div#product-image a[data-thumbnail]");
+    var prodImgA =
+        document.querySelector("div#product-image a[data-thumbnail]");
     if (prodImgA != null) {
       disk.pictureUrl = prodImgA.attributes["data-thumbnail"];
     }
@@ -50,11 +46,9 @@ class InformatiqueDiskDetailParser implements PageWorker {
     var tables = document.querySelectorAll("table#details");
 
     for (var table in tables) {
-
       var rows = table.querySelectorAll("tr");
 
       for (var row in rows) {
-
         var label = row.querySelector("strong");
 
         if (label == null) {
@@ -64,15 +58,17 @@ class InformatiqueDiskDetailParser implements PageWorker {
         } else if (label.text == "Fabrikantcode") {
           disk.mpn = row.querySelector("tr:last-child span").text;
         }
+
+        if(disk.ean != null && disk.mpn != null){
+          break;
+        }
       }
     }
 
-    var querySelector = document.querySelector("#description").querySelector("span");
+    var querySelector =
+        document.querySelector("#description").querySelector("span");
     var innerHtml = querySelector.innerHtml;
-    var lastIndexOf = innerHtml.lastIndexOf(" ");
-    var diskConnector = innerHtml.substring(lastIndexOf, innerHtml.length).trim();
-
-    disk.connectors.add(new Connector(diskConnector, "STORAGE"));
+    disk.connectors.add(new Connector(innerHtml, "STORAGE"));
 
     await postProduct(disk);
     await sleepRnd();
