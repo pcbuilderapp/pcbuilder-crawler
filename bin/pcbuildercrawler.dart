1453e63873b287734a31ae07a98333a7a256d7d5
Stefan Ladage <sladage@gmail.com>
2017-01-16 22:08:19 +0100

    Config reader.

--- bin/pcbuildercrawler.dart
+++ bin/pcbuildercrawler.dart
@@ -1,7 +1,17 @@
 import 'package:pcbuilder.crawler/alternate/crawlternate.dart';
 import 'package:pcbuilder.crawler/informatique/informacrawl.dart';
+import 'package:pcbuilder.crawler/config.dart';
+import 'dart:io';
+import 'dart:async';
 
 main(List<String> args) async {
+  File cfgFile = new File("config.yaml");
+  try {
+    String yamlSrc = await cfgFile.readAsString();
+    config.init(yamlSrc);
+  } catch (e) {
+    print(e);
+  }
   Informacrawl.crawlInformatique();
   Crawlternate.crawlAlternate();
 }
