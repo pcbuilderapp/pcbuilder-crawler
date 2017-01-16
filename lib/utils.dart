b7961e0f1b284720a0cbc2e4a1213a7086220405
Daniel de Witte <danieldewitte@gmail.com>
2017-01-16 13:52:26 +0100

    utils readability improvements

--- lib/utils.dart
+++ lib/utils.dart
@@ -9,8 +9,9 @@ import 'package:pcbuilder.crawler/configuration.dart';
 
 JsonEncoder jsonEncoder = new JsonEncoder.withIndent("  ");
 
-///sleep a random amount of time without invoking a lock///
+///sleep a random amount of time without invoking a lock
 Future sleepRnd() {
+
   Completer c = new Completer();
   new Timer(new Duration(milliseconds: (new Random().nextDouble() * 300 + 200).round()),(){
     c.complete();
@@ -18,20 +19,25 @@ Future sleepRnd() {
   return c.future;
 }
 
-///create a new Shop in the backend///
+///create a new Shop in the backend
 void createShop(String name, String url) {
+
   postRequest(backendServerUrl + createShopUrl,
       jsonEncoder.convert(new Shop(name, url, "")));
 }
 
 String removeTip(String productName) {
+
     return productName.replaceAll("(tip)", "");
 }
 
-///convert price from String to double///
+///convert price from String to double
 double price(String price) {
+
   List<int> temp = [];
+
   for (int codeUnit in price.codeUnits) {
+
     if (codeUnit >= 48 && codeUnit <= 57) {
       temp.add(codeUnit);
     } else if (codeUnit == 44) {
@@ -40,32 +46,42 @@ double price(String price) {
       temp.add(48);
     }
   }
+
   return double.parse(new String.fromCharCodes(temp));
 }
 
-///create headers for HTTP calls to look like a browser///
+///create headers for HTTP calls to look like a browser
 Map getHTTPHeaders(String url, {String referrer, Map<String,String> cookies}) {
+
   Map headers = {};
+
   headers["Host"] = Uri.parse(url).host;
   headers["Cache-Control"] = "max-age=0";
   headers["Upgrade-Insecure-Requests"] = "1";
   headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36";
   headers["Accept"] = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8";
+
   if (referrer != null) headers["Referer"] = referrer;
+
   headers["Accept-Encoding"] = "gzip, deflate, sdch, br";
   headers["Accept-Language"] = "nl-NL,nl;q=0.8,en-US;q=0.6,en;q=0.4,de-DE;q=0.2";
+
   if (cookies != null) {
+
     String cookiesstr = "";
+
     for (String key in cookies.keys) {
       cookiesstr += "$key=\"${cookies[key]}\";";
     }
+
     headers["Cookie"] = cookiesstr;
   }
+
   headers["DNT"] = "1";
   return headers;
 }
 
-///add a Product to the backend///
+///add a Product to the backend
 void postProduct(Product product) {
 
   if (product.connectors.length > 0) {
@@ -76,7 +92,7 @@ void postProduct(Product product) {
   }
 }
 
-/// post data on a REST service URL and print the response///
+///post data on a REST service URL and print the response
 void postRequest(String url, String json) {
 
     if (printProducts) {
