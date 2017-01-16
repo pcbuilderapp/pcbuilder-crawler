cf7457804c132ec0165a1ab147320a42694294b8
Mehmet Sozen <sozen.m@hotmail.com>
2017-01-16 22:23:03 +0100

    Added connectorChecker

--- lib/configuration.dart
+++ lib/configuration.dart
@@ -9,26 +9,29 @@ final String createShopUrl = "/shop/create";
 final String informatiqueName = "Informatique";
 final String informatiqueUrl = "www.informatique.nl";
 
-
 //Informatique product pages//
 
 final List informatiqueDiskUrls = [
   "http://www.informatique.nl/?m=usl&g=026&view=6&&sort=pop&pl=119",
   "http://www.informatique.nl/?m=usl&g=028&view=6&&sort=pop&pl=32",
   "http://www.informatique.nl/?m=usl&g=559&view=6&&sort=pop&pl=221",
-  "http://www.informatique.nl/?m=art&g=170&view=6&&sort=pop&pl=22"];
+  "http://www.informatique.nl/?m=art&g=170&view=6&&sort=pop&pl=22"
+];
 
 final List informatiqueProcessorUrls = [
   "http://www.informatique.nl/?m=usl&g=611&pl=500",
-  "http://www.informatique.nl/?m=usl&g=218&pl=500"];
+  "http://www.informatique.nl/?m=usl&g=218&pl=500"
+];
 
 final List informatiquePowerSupplyUnitUrls = [
-  "http://www.informatique.nl/?m=usl&g=171&view=6&&sort=pop&pl=196"];
+  "http://www.informatique.nl/?m=usl&g=171&view=6&&sort=pop&pl=196"
+];
 
 final List informatiqueMemoryUrls = [
   "http://www.informatique.nl/?m=usl&g=522&view=6&&sort=pop&pl=277",
   "http://www.informatique.nl/?m=usl&g=194&view=6&&sort=pop&pl=21",
-  "http://www.informatique.nl/opslag_en_geheugen/geheugenmodules/ddr_modules/c019-h037-g077/"];
+  "http://www.informatique.nl/opslag_en_geheugen/geheugenmodules/ddr_modules/c019-h037-g077/"
+];
 
 final List informatiqueMotherboardUrls = [
   "http://www.informatique.nl/?m=usl&g=726&view=6&&sort=pop&pl=46",
@@ -39,21 +42,22 @@ final List informatiqueMotherboardUrls = [
   "http://www.informatique.nl/pc_componenten/moederborden/socket_am1_(amd)/c001-h033-g717/",
   "http://www.informatique.nl/?m=usl&g=655&view=6&&sort=pop&pl=30",
   "http://www.informatique.nl/pc_componenten/moederborden/socket_fm2_(amd)/c001-h033-g686/",
-  "http://www.informatique.nl/pc_componenten/moederborden/on-board_cpu/c001-h033-g558/"];
+  "http://www.informatique.nl/pc_componenten/moederborden/on-board_cpu/c001-h033-g558/"
+];
 
 final List informatiqueVideocardUrls = [
   "http://www.informatique.nl/?m=usl&g=166&view=6&&sort=pop&pl=211",
-  "http://www.informatique.nl/?m=usl&g=188&view=6&&sort=pop&pl=53"];
+  "http://www.informatique.nl/?m=usl&g=188&view=6&&sort=pop&pl=53"
+];
 
 final List informatiqueCaseUrls = [
-  "http://www.informatique.nl/?m=usl&g=004&view=6&&sort=pop&pl=547"];
-
+  "http://www.informatique.nl/?m=usl&g=004&view=6&&sort=pop&pl=547"
+];
 
 //Alternate information//
 
 final String alternateName = "Alternate";
 final String alternateUrl = "www.alternate.nl";
-
 
 //Alternate product pages//
 
@@ -61,29 +65,38 @@ final List alternateDiskUrls = [
   "https://www.alternate.nl/Hardware/html/listings/1472811138409?size=500",
   "https://www.alternate.nl/Harde-schijven-intern/SATA-2-5-inch?size=500",
   "https://www.alternate.nl/Harde-schijven-intern/SATA-3-5-inch?size=500",
-  "https://www.alternate.nl/Harde-schijven-intern/Hybride?size=500"];
+  "https://www.alternate.nl/Harde-schijven-intern/Hybride?size=500"
+];
 
 final List alternateProcessorUrls = [
-"https://www.alternate.nl/Processoren/Desktop/Alle-processoren?size=500"];
+  "https://www.alternate.nl/Processoren/Desktop/Alle-processoren?size=500"
+];
 
 final List alternatePowerSupplyUnitUrls = [
-  "https://www.alternate.nl/Hardware-Componenten-Voedingen-Alle-voedingen/html/listings/11604?size=500"];
+  "https://www.alternate.nl/Hardware-Componenten-Voedingen-Alle-voedingen/html/listings/11604?size=500"
+];
 
 final List alternateMemoryUrls = [
   "https://www.alternate.nl/Geheugen/DDR4?size=500",
   "https://www.alternate.nl/Geheugen/DDR3?size=500",
   "https://www.alternate.nl/Geheugen/DDR2?size=500",
   "https://www.alternate.nl/Geheugen/DDR?size=500",
-  "https://www.alternate.nl/Geheugen/SDRAM?size=500"];
+  "https://www.alternate.nl/Geheugen/SDRAM?size=500"
+];
 
 final List alternateMotherboardUrls = [
   "https://www.alternate.nl/Moederborden/AMD?size=500",
-  "https://www.alternate.nl/Moederborden/Intel?size=500"];
+  "https://www.alternate.nl/Moederborden/Intel?size=500"
+];
 
 final List alternateVideocardUrls = [
   "https://www.alternate.nl/Grafische-kaarten/NVIDIA-GeForce?size=500",
-  "https://www.alternate.nl/Grafische-kaarten/AMD-Radeon?size=500"];
+  "https://www.alternate.nl/Grafische-kaarten/AMD-Radeon?size=500"
+];
 
 final List alternateCaseUrls = [
-  "https://www.alternate.nl/Behuizingen/Alle-behuizingen?size=500"];
+  "https://www.alternate.nl/Behuizingen/Alle-behuizingen?size=500"
+];
 
+//White list for disk type connectors
+final List whiteListDisks = ["SATA", "M.2"];
