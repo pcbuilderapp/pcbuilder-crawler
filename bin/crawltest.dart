cde4ade3faceb8ad77420f4d1ff841aa6f4b23e2
Stefan Ladage <sladage@gmail.com>
2017-01-27 21:11:24 +0100

    Crawler test runner added.

---
+++ bin/crawltest.dart
@@ -0,0 +1,22 @@
+import 'package:pcbuilder.crawler/alternate/crawlternate.dart';
+import 'package:pcbuilder.crawler/informatique/informacrawl.dart';
+import 'package:pcbuilder.crawler/config.dart';
+import 'dart:io';
+import 'dart:async';
+
+main(List<String> args) async {
+  File cfgFile = new File("config.yaml");
+
+  try {
+    String yamlSrc = await cfgFile.readAsString();
+    config.init(yamlSrc);
+  } catch (e) {
+    print(e);
+  }
+
+  Crawlternate crawlternate = new Crawlternate();
+  Informacrawl informacrawl = new Informacrawl();
+
+  crawlternate.crawlShop();
+  informacrawl.crawlShop();
+}
